caa5587da116d9837b6d219a006dd119b2203ecd
Matt Triff <matt.triff@gmail.com>
2020-03-19 14:39:08 -0400

    Added case changing stream to lexer

--- parse.js
+++ parse.js
@@ -1,11 +1,13 @@
 const antlr4 = require('antlr4');
+const {CaseChangingStream} = require("./CaseChangingStream");
 const TSqlParser = require('./output/tsql/TSqlParser.js');
 const TSqlLexer = require('./output/tsql/TSqlLexer.js');
 
-const input = 'SELECT * FROM tableName';
+const input = 'SELECT * FROM table';
 
 const chars = new antlr4.InputStream(input);
-const lexer = new TSqlLexer.TSqlLexer(chars);
+const caseChangingCharStream = new CaseChangingStream(chars, true);;
+const lexer = new TSqlLexer.TSqlLexer(caseChangingCharStream);
 
 lexer.strictMode = false; // do not use js strictMode
 
